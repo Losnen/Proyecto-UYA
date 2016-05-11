d7a4c832eebc93640c30e728612e087d6470378e
Samuel Ramos <alu0100815146@ull.edu.es>
2016-05-11 13:15:48 +0100

    Simplificando el servidor

--- app.js
+++ app.js
@@ -3,27 +3,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const lay = require('express-ejs-layouts');
 
 app.set('port',(process.env.PORT || 8080));
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.use(lay);
-
 app.use(express.static(__dirname + '/public'));
-
-app.get('/',(request, response) => {
-  response.render("index");
-});
-
-app.get('/trayecto',(request, response) => {
-  response.render("trayectos");
-});
-
-app.get('/contacto',(request, response) => {
-  response.render("contacto");
-});
 
 app.listen(app.get('port'), () => {
   console.log(`Node app running on localhost 8080`);
