9de4ddf8befa1e4f5193d4e3310e49507de25d1a
Samuel Ramos <alu0100815146@ull.edu.es>
2016-05-12 08:44:12 +0100

    Comenzando despliegue de la db mysql

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 "use strict"
 
 const express = require('express');
-const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({ // Mysql Connection
+    host : 'localhost',
+    user : 'root',
+    password : 'uya1516',
+    database : 'coches',
+});
+
+
 
 app.set('port',(process.env.PORT || 8080));
-
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
